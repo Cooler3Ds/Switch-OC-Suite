0a92a973b999f74393e2de346775b074227f1693
halop <halop8@gmail.com>
2023-11-10 16:04:14 +0100

    Update pcv.cpp

--- Source/Atmosphere/stratosphere/loader/source/oc/pcv/pcv.cpp
+++ Source/Atmosphere/stratosphere/loader/source/oc/pcv/pcv.cpp
@@ -115,7 +115,7 @@ void SafetyCheck() {
         { C.commonEmcMemVolt,    1100'000, 1350'000 },
         { C.eristaCpuMaxVolt,        1100,     1300 },
         { C.eristaEmcMaxClock,   1600'000, 2131'200 },
-        { C.marikoCpuMaxVolt,        1100,     1300 },
+        { C.marikoCpuMaxVolt,        1100,     1235 },
         { C.marikoEmcMaxClock,   1600'000, 3000'000 },
         { C.marikoEmcVddqVolt,    550'000,  650'000 },
         { eristaCpuDvfsMaxFreq,  1785'000, 3000'000 },
